e83b72d4ae9c8c771da7c7b1ba8a62f4ae258949
GarifullinII <ildaridrisovich@gmail.com>
2024-09-11 23:13:20 +0400

    add group, extensions

---
+++ CalculatorLayout/Extensions/UIKit/UIColor+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor+Extensions.swift
+//  CalculatorLayout
+//
+//  Created by Ildar Garifullin on 11/09/2024.
+//
+
+import Foundation
---
+++ CalculatorLayout/Extensions/UIKit/UIStackView+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIStackView+Extensions.swift
+//  CalculatorLayout
+//
+//  Created by Ildar Garifullin on 11/09/2024.
+//
+
+import Foundation
